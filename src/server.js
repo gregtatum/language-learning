00a95435831d8bd03fbd2aecc50ae356b9653802
Greg Tatum <tatum.creative@gmail.com>
2022-04-04 21:17:35 -0500

    Add .csv results

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 
 /** @typedef {import("./types").SegmenterClass} SegmenterClass */
 /** @typedef {import("./types").Stem} Stem */
+/** @typedef {import("express").Response} Response */
 
 /** @type {SegmenterClass} */
 const Segmenter =
@@ -16,6 +17,7 @@ const Segmenter =
 require('dotenv').config();
 
 const app = express();
+app.use(require('morgan')('tiny'));
 const host = process.env.HOST;
 const port = process.env.PORT;
 if (!host) {
@@ -27,7 +29,13 @@ if (!port) {
 
 app.get('/', (req, res) => {
   res.json({
-    endpoints: [{ path: '/stem', description: 'Stem a list of words.' }],
+    endpoints: [
+      {
+        paths: ['/:locale/stem', '/:locale/stem.csv'],
+        description: 'Stem a list of words.',
+        getArgs: ['text'],
+      },
+    ],
   });
 });
 
@@ -44,73 +52,124 @@ function validateLocale(locale) {
   return null;
 }
 
+/**
+ * @param {Response} res
+ * @param {string} localeParam
+ * @returns {Promise<null | {locale: string, hunspell: Nodehun}>}
+ */
+async function getHunspell(res, localeParam) {
+  const locale = validateLocale(localeParam);
+  if (!locale) {
+    res.status(400);
+    res.send('invalid locale');
+    return null;
+  }
+  let affix, dictionary;
+  try {
+    affix = await fs.readFile(
+      path.join(__dirname, '../dictionaries', locale, 'index.aff'),
+    );
+    dictionary = await fs.readFile(
+      path.join(__dirname, '../dictionaries', locale, 'index.dic'),
+    );
+  } catch (error) {
+    res.status(404);
+    res.json(`The locale "${locale} could not be found."`);
+    return null;
+  }
+  return { hunspell: new Nodehun(affix, dictionary), locale };
+}
+
+/**
+ * @param {string} locale
+ * @param {string} text
+ * @param {Nodehun} hunspell
+ * @returns {Promise<Stem[]>}
+ */
+async function getStems(locale, text, hunspell) {
+  const sentenceSegmenter = new Segmenter(locale, {
+    granularity: 'sentence',
+  });
+  const wordSegmenter = new Segmenter(locale, { granularity: 'word' });
+  /** @type {Map<string, Stem>} */
+  const stemsByStem = new Map();
+  for (const { segment: sentence } of sentenceSegmenter.segment(text)) {
+    for (const segment of wordSegmenter.segment(sentence)) {
+      if (!segment.isWordLike) {
+        continue;
+      }
+      let [stemString] = await hunspell.stem(segment.segment);
+      if (!stemString) {
+        stemString = '(unknown)';
+      }
+      let stem = stemsByStem.get(stemString);
+      if (!stem) {
+        stem = {
+          stem: stemString,
+          frequency: 0,
+          tokens: [],
+          sentences: [],
+        };
+        stemsByStem.set(stemString, stem);
+      }
+      stem.tokens.push(segment.segment);
+      stem.sentences.push(sentence);
+      stem.frequency++;
+    }
+  }
+
+  const stems = [...stemsByStem.values()];
+  return stems.sort((a, b) => b.frequency - a.frequency);
+}
+
 app.get('/:locale/stem', async (req, res) => {
   try {
     const { text } = req.query;
-    const locale = validateLocale(req.params.locale);
-    if (!locale) {
-      res.status(400);
-      res.json('invalid locale');
-      return;
-    }
     if (typeof text !== 'string') {
       res.status(400);
-      res.json('"text" must be a string');
+      res.send('"text" must be a string');
       return;
     }
-    let affix, dictionary;
-    try {
-      affix = await fs.readFile(
-        path.join(__dirname, '../dictionaries', locale, 'index.aff'),
-      );
-      dictionary = await fs.readFile(
-        path.join(__dirname, '../dictionaries', locale, 'index.dic'),
-      );
-    } catch (error) {
-      res.status(404);
-      res.json(`The locale "${locale} could not be found."`);
+
+    const result = await getHunspell(res, req.params.locale);
+    if (!result) {
       return;
     }
-    const hunspell = new Nodehun(affix, dictionary);
+    const { locale, hunspell } = result;
 
-    const sentenceSegmenter = new Segmenter(locale, {
-      granularity: 'sentence',
-    });
-    const wordSegmenter = new Segmenter(locale, { granularity: 'word' });
-    /** @type {Map<string, Stem>} */
-    const stemsByStem = new Map();
-    for (const { segment: sentence } of sentenceSegmenter.segment(text)) {
-      for (const segment of wordSegmenter.segment(sentence)) {
-        if (!segment.isWordLike) {
-          continue;
-        }
-        const [stemString] = await hunspell.stem(segment.segment);
-        if (!stemString) {
-          continue;
-        }
-        let stem = stemsByStem.get(stemString);
-        if (!stem) {
-          stem = {
-            stem: stemString,
-            frequency: 0,
-            tokens: [],
-            sentences: [],
-          };
-          stemsByStem.set(stemString, stem);
-        }
-        stem.tokens.push(segment.segment);
-        stem.sentences.push(sentence);
-        stem.frequency++;
-      }
-    }
-
-    const stems = [...stemsByStem.values()];
-    stems.sort((a, b) => b.frequency - a.frequency);
-
-    res.json({ stems });
+    res.json({ stems: await getStems(locale, text, hunspell) });
   } catch (error) {
     res.status(501);
-    res.json('Internal server error');
+    res.send('Internal server error');
+    console.error(error);
+  }
+});
+
+app.get('/:locale/stem.csv', async (req, res) => {
+  try {
+    const { text } = req.query;
+    if (typeof text !== 'string') {
+      res.status(400);
+      res.send('"text" must be a string');
+      return;
+    }
+
+    const result = await getHunspell(res, req.params.locale);
+    if (!result) {
+      return;
+    }
+    const { locale, hunspell } = result;
+    res.set({ 'content-type': 'text/csv; charset=utf-8' });
+
+    const stems = await getStems(locale, text, hunspell);
+    res.write(`Stem:,Frequency:,Words:\n`);
+    for (const stem of stems) {
+      res.write(`${stem.stem},${stem.frequency},${stem.tokens.join(' ')}\n`);
+    }
+    res.send();
+  } catch (error) {
+    res.status(501);
+    res.send('Internal server error');
     console.error(error);
   }
 });
